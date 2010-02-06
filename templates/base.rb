65151ab56d3139fe13243505cda2636f811f4ea0
Miha Rebernik <rasmajx@gmail.com>
2010-02-06 23:45:58 +0100

    Initial import.

---
+++ templates/base.rb
@@ -0,0 +1,46 @@
+# Truncate the nasty big default README file.
+run "echo 'Comming soon' > README"
+
+# Have our bit of gem.
+gem 'haml'
+gem 'RedCloth'
+gem 'resource_controller'
+gem 'will_paginate'
+rake "gems:install"
+
+# Install HAML
+run "haml --rails ."
+
+# Get the standardista
+plugin "standardista", :git => "git://github.com/mislav/standardista.git"
+
+# RSpec question.
+if yes?("Do you want to use RSpec?")
+  plugin "rspec", :git => "git://github.com/dchelimsky/rspec.git"
+  plugin "rspec-rails", :git => "git://github.com/dchelimsky/rspec-rails.git"
+  generate :rspec
+end
+
+# Setup Git.
+git :init
+file ".gitignore", <<-END
+.DS_Store
+.eprj
+config/initializers/gmaps.rb
+log/*.log
+tmp/**/*
+nbproject
+config/database.yml
+db/*.sqlite3
+END
+run "touch tmp/.gitignore log/.gitignore vendor/.gitignore"
+run "cp config/database.yml config/example_database.yml"
+
+# Generate the static pages controller and setup the root.
+generate :controller, "pages index"
+route "map.root :controller => :pages"
+run "rm public/index.html"
+
+# Finally add to repo and commit.
+git :add => "."
+git :commit => "-m 'Initial import.'"
